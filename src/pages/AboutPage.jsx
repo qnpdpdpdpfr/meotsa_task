bd543ac9b516caa8667d233ffd7da9e42eaa2738
qnpdpdpdpfr <amollohaji@gmail.com>
2026-05-18 18:39:39 +0900

    final

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,13 +1,25 @@
 // src/pages/AboutPage.jsx
+import './AboutPage.css'
+
 export default function AboutPage() {
     return (
-        <div>
-            <h1>소개 페이지</h1>
-            <p>홍소담</p>
-            <p>Social Science & AI 융합전공</p>
-            <p>3학년</p>
-            <p>INTP</p>
-            <p>프론트앤드 개발을 배우고 있습니다.</p>
+        <div className="about-page-container">
+
+            <h1 className="about-title">소개 페이지</h1>
+            
+            <div className="about-card">
+                <p className="myname">홍소담</p>
+                
+                <div className="info-tags">
+                    <span className="inftag">Social Science & AI 융합전공</span>
+                    <span className="info-tag">3학년</span>
+                    <span className="info-tag mbti">INTP</span>
+                </div>
+                
+                <p className="about-description">
+                    프론트엔드 개발을 배우고 있습니다. 
+                </p>
+            </div>
         </div>
     );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router';
 import './HomePage.css';
 import PostsPage from './PostsPage'; 
 
-export default function HomePage({ posts }) {
+export default function HomePage() {
     return (
         <div className="home-container">
             <h1 className="home-title">
@@ -14,10 +14,6 @@ export default function HomePage({ posts }) {
                 <p>블로그에서 여러 정보를 얻어가세요.</p>
             </div>
 
-
-            <div className="home-posts-section">
-                <PostsPage posts={posts} />
-            </div>
         </div>
     );
 }
